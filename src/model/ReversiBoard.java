b94af1fa4e9478fa88fbaf846954b0d9775e2096
Shreyas Khandekar <shreyaskhandekar@email.arizona.edu>
2021-03-31 14:58:39 -0700

    making Reversi and ReversiBoard classes

---
+++ src/model/ReversiBoard.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ * @author Shreyas Khandekar
+ *
+ */
+public class ReversiBoard implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/view/Reversi.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package view;
+
+/**
+ * @author Shreyas Khandekar
+ *
+ */
+public class Reversi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
