4f7e589ab28bd109f3e9797e3c54bd4d6e76120b
Mihir Yadav <Mihiry4@gmail.com>
2021-04-05 01:29:54 -0700

    0.6.1 wrote basic GUI skeleton code

--- src/view/ReversiView.java
+++ src/view/ReversiView.java
@@ -14,6 +14,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import model.ReversiModel;
@@ -35,30 +37,40 @@ public class ReversiView extends Application implements Observer {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-	BorderPane pane = new BorderPane();
-	GridPane masterGridPane = new GridPane();
-    pane.setCenter(masterGridPane);
-    masterGridPane.setStyle("-fx-background-color: green");
-	masterGridPane.add(addGridPane(), 1, 1);
-
-//    for(int i = 0;i<8;i++) {
-//    }
-    
-     // stage setup
-     Scene scene = new Scene(pane, 400, 400);
-	 stage.setScene(scene);
-	 stage.setTitle("Mastermind");
-	 stage.show();
+		BorderPane pane = new BorderPane();
+		GridPane masterGridPane = new GridPane();
+	    pane.setCenter(masterGridPane);
+	    masterGridPane.setStyle("-fx-background-color: green");
+		
+	    for(int i = 0;i<8;i++) {
+	    	for(int j = 0; j<8;j++) {
+	    		masterGridPane.add(addStackPane(), i, j);
+	    	}
+	    }
+	    masterGridPane.setPadding(new Insets(40, 10, 40, 16));
+	    
+	     // stage setup
+	     Scene scene = new Scene(pane, 400, 430);
+		 stage.setScene(scene);
+		 stage.setTitle("Reversi");
+		 stage.show();
 	}
 	
-	private StackPane addGridPane() {
+	private StackPane addStackPane() {
 		//Rectangle(double x, double y, double width, double height)
-		Rectangle s = new Rectangle(0,0,40,40);
+		Rectangle s = new Rectangle(0,0,45,45);
+		Circle c = new Circle(20);
 		StackPane stackPane = new StackPane();
-		s.setStyle("-fx-background-color: blue");
+		s.setStyle("-fx-fill: green; -fx-stroke: black; -fx-stroke-width: 1;");
 		stackPane.getChildren().add(s);
+		stackPane.getChildren().add(c);
 		return stackPane;
 		
+	}
+	
+	private Paint getPaint(String s) {
+		Paint paint1 = Paint.valueOf(s);
+		return paint1;
 	}
 
 	@Override
